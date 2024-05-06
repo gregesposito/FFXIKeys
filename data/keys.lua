55690ecf942fafddf8d75052fca7e4836720b897
Greg Esposito <gregesposito@mac.com>
2024-05-06 11:42:06 -0400

    Update for Dial Keys

--- data/keys.lua
+++ data/keys.lua
@@ -32,6 +32,9 @@ Keys.Values['']   = { id = 0000, idx = 0, en = '' }
 
 -- Keys
 Keys.Values[8973] = { id = 8973, idx = 01, en = 'SP Gobbie Key' }
+Keys.Values[9274] = { id = 9274, idx = 01, en = 'Dial Key #ANV' }
+Keys.Values[9218] = { id = 9218, idx = 01, en = 'Dial Key #Fo' }
+Keys.Values[9217] = { id = 9217, idx = 01, en = 'Dial Key #Ab' }
 Keys.Values[4181] = { id = 4181, idx = 02, en = 'Warp Scroll' }
 Keys.Values[5945] = { id = 5945, idx = 12, en = 'Prize Powder' }
 
